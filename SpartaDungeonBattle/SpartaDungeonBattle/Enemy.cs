2b38e97a0bfda7de6a697378be13ba8f3a036d80
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-04-29 12:23:53 +0900

    몬스터 정의
    몬스터 정의

---
+++ SpartaDungeonBattle/SpartaDungeonBattle/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeonBattle
+{
+    public class Enemy
+    {
+        protected int level,hp,atk;
+        protected string name;
+
+        public void Display()
+        {
+            Console.WriteLine($"Lv.{level} {name} HP {hp}");
+        }
+    }
+
+    public class Minion : Enemy
+    {
+        public Minion()
+        {
+            level = 2;
+            hp = 15;
+            atk = 5;
+        }
+    }
+    public class SiegeMinion : Enemy
+    {
+        public SiegeMinion()
+        {
+            level = 5;
+            hp = 25;
+            atk = 8;
+        }
+    }
+    public class HollowWorm : Enemy
+    {
+        public HollowWorm()
+        {
+            level = 3;
+            hp = 10;
+            atk = 9;
+        }
+    }
+}
